3c97aa67adf2e94e78a4f4c4d31c3cd50d7d5b3a
add-bakkers <add.re.0828@gmail.com>
2022-03-14 20:06:07 +0900

    update:network

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,11 +55,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://localhost:3000",
-    "https://127.0.0.1:3000",
-    "https://127.0.0.1:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://192.168.2.20:3000",
 ]
 
 ROOT_URLCONF = 'backend.urls'
